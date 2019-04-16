9f9b1778390ea03c830e6f27266647248b6ec0a7
skreimeyer <samuel.kreimeyer@gmail.com>
2019-04-16 13:58:06 -0500

    examples reflect lint-friendly updates

--- output_sample_github.go
+++ output_sample_github.go
@@ -1,36 +1,33 @@
 type root struct {
 Login string `json:"login"`
 Id int `json:"id"`
-Node_Id string `json:"node_id"`
-Avatar_Url string `json:"avatar_url"`
-Gravatar_Id string `json:"gravatar_id"`
+NodeId string `json:"node_id"`
+AvatarUrl string `json:"avatar_url"`
+GravatarId string `json:"gravatar_id"`
 Url string `json:"url"`
-Html_Url string `json:"html_url"`
-Followers_Url string `json:"followers_url"`
-Following_Url string `json:"following_url"`
-Gists_Url string `json:"gists_url"`
-Starred_Url string `json:"starred_url"`
-Subscriptions_Url string `json:"subscriptions_url"`
-Organizations_Url string `json:"organizations_url"`
-Repos_Url string `json:"repos_url"`
-Events_Url string `json:"events_url"`
-Received_Events_Url string `json:"received_events_url"`
+HtmlUrl string `json:"html_url"`
+FollowersUrl string `json:"followers_url"`
+FollowingUrl string `json:"following_url"`
+GistsUrl string `json:"gists_url"`
+StarredUrl string `json:"starred_url"`
+SubscriptionsUrl string `json:"subscriptions_url"`
+OrganizationsUrl string `json:"organizations_url"`
+ReposUrl string `json:"repos_url"`
+EventsUrl string `json:"events_url"`
+ReceivedEventsUrl string `json:"received_events_url"`
 Type string `json:"type"`
-Site_Admin bool `json:"site_admin"`
+SiteAdmin bool `json:"site_admin"`
 Name string `json:"name"`
 Company string `json:"company"`
 Blog string `json:"blog"`
 Location string `json:"location"`
-Email nil `json:"email"`
-Hireable nil `json:"hireable"`
-Bio nil `json:"bio"`
-Public_Repos int `json:"public_repos"`
-Public_Gists int `json:"public_gists"`
+Email int `json:"email"`
+Hireable int `json:"hireable"`
+Bio int `json:"bio"`
+PublicRepos int `json:"public_repos"`
+PublicGists int `json:"public_gists"`
 Followers int `json:"followers"`
 Following int `json:"following"`
-Created_At string `json:"created_at"`
-Updated_At string `json:"updated_at"`
+CreatedAt string `json:"created_at"`
+UpdatedAt string `json:"updated_at"`
 }
-
-
-
